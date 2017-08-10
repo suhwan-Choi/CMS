141893067185fcd825a6c1e59c1f948c9f1f85ea
suhwan-Choi <suhwan-Choi@fashiongo.net>
2017-08-10 15:22:04 +0900

    [Mason] UserManager
    -- change getter searchStartDate, searchEndDate
      -- String -> Date

--- src/main/java/com/fashiongo/cms/param/UserManagerListParam.java
+++ src/main/java/com/fashiongo/cms/param/UserManagerListParam.java
@@ -1,5 +1,7 @@
 package com.fashiongo.cms.param;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class UserManagerListParam {
@@ -9,57 +11,73 @@ public class UserManagerListParam {
 	private String keywordText;
 	private Boolean active;
 	private String searchDateType;
-	private Date searchStartDate;
-	private Date searchEndDate;
+	private String searchStartDate;
+	private String searchEndDate;
 
 	public Integer getPn() {
 		return pn;
 	}
+
 	public void setPn(Integer pn) {
 		this.pn = pn;
 	}
+
 	public Integer getPs() {
 		return ps;
 	}
+
 	public void setPs(Integer ps) {
 		this.ps = ps;
 	}
+
 	public String getKeywordType() {
 		return keywordType;
 	}
+
 	public void setKeywordType(String keywordType) {
 		this.keywordType = keywordType;
 	}
+
 	public String getKeywordText() {
 		return keywordText;
 	}
+
 	public void setKeywordText(String keywordText) {
 		this.keywordText = keywordText;
 	}
+
 	public Boolean getActive() {
 		return active;
 	}
+
 	public void setActive(Boolean active) {
 		this.active = active;
 	}
+
 	public String getSearchDateType() {
 		return searchDateType;
 	}
+
 	public void setSearchDateType(String searchDateType) {
 		this.searchDateType = searchDateType;
 	}
-	public Date getSearchStartDate() {
-		return searchStartDate;
+
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+	public Date getSearchStartDate() throws ParseException {
+		return sdf.parse(this.searchStartDate);
 	}
-	public void setSearchStartDate(Date searchStartDate) {
+
+	public void setSearchStartDate(String searchStartDate) {
 		this.searchStartDate = searchStartDate;
 	}
-	public Date getSearchEndDate() {
-		return searchEndDate;
+
+	public Date getSearchEndDate() throws ParseException {
+		return sdf.parse(this.searchEndDate);
 	}
-	public void setSearchEndDate(Date searchEndDate) {
+
+	public void setSearchEndDate(String searchEndDate) {
 		this.searchEndDate = searchEndDate;
 	}
 
-	
 }

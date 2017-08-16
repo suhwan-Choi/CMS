635210515e0d04c8e0edaac6d6dbae867496efc6
myounghoi <myounghoi@fashiongo.net>
2017-08-17 08:16:15 +0900

    [Reo] MyAccount Api Add

--- src/main/java/com/fashiongo/cms/service/MyAccountService.java
+++ src/main/java/com/fashiongo/cms/service/MyAccountService.java
@@ -1,11 +1,29 @@
 package com.fashiongo.cms.service;
 
+import javax.persistence.StoredProcedureQuery;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import com.fashiongo.cms.model.ProcedureResult;
+import com.fashiongo.cms.param.MyAccountListParam;;
 
 @Service
 public class MyAccountService extends CommonService {
 	private static Logger logger = LoggerFactory.getLogger(MyAccountService.class);
 
+	public ProcedureResult modifyUserPassword(MyAccountListParam myAccountListParam) {
+		StoredProcedureQuery query = entityManager.createNamedStoredProcedureQuery("upWeb_ModifyUserPassword");
+		
+		query.setParameter("UserID", myAccountListParam.getUserID());
+		query.setParameter("UserPassword", myAccountListParam.getUserPassword());
+		query.setParameter("WorkedBy", myAccountListParam.getWorkedBy());
+
+		query.execute();
+
+		ProcedureResult procedureResult = new ProcedureResult();
+		procedureResult.setResultCode((Integer) query.getOutputParameterValue("ResultCode"));
+		procedureResult.setErrorMessage((String) query.getOutputParameterValue("ErrorMessage"));
+		return procedureResult;
+	}
 }
--- src/main/java/com/fashiongo/cms/web/MyAccountController.java
+++ src/main/java/com/fashiongo/cms/web/MyAccountController.java
@@ -1,9 +1,15 @@
 package com.fashiongo.cms.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fashiongo.cms.common.JSONResponse;
+import com.fashiongo.cms.model.ProcedureResult;
+import com.fashiongo.cms.param.MyAccountListParam;
 import com.fashiongo.cms.service.MyAccountService;
 
 @RestController
@@ -12,4 +18,11 @@ public class MyAccountController {
 	
 	@Autowired
 	private MyAccountService myAccountService;
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public JSONResponse<ProcedureResult> save(@RequestBody MyAccountListParam myAccountListParam) throws Exception {
+		JSONResponse<ProcedureResult> jsonResponse = new JSONResponse<>();
+		jsonResponse.setData(myAccountService.modifyUserPassword(myAccountListParam));
+		return jsonResponse;
+	}
 }

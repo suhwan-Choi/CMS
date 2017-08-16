ff46d43051d8e498796b1aff4598d5ab926c075f
taehyung.lee <taehyung.lee@fashiongo.net>
2017-08-17 08:48:04 +0900

    [Ian Lee] Modify API

--- src/main/java/com/fashiongo/cms/param/MyAccountListParam.java
+++ src/main/java/com/fashiongo/cms/param/MyAccountListParam.java
@@ -1,44 +1,31 @@
 package com.fashiongo.cms.param;
 
-import java.util.Date;
-
 public class MyAccountListParam{
 	private Integer userID;
-	private String userAccount;
-	private String userName;
 	private String userPassword;
 	private Integer workedBy;
 	
 	public Integer getWorkedBy() {
 		return workedBy;
 	}
+	
 	public void setWorkedBy(Integer workedBy) {
 		this.workedBy = workedBy;
 	}
+	
 	public Integer getUserID() {
 		return userID;
 	}
+	
 	public void setUserID(Integer userID) {
 		this.userID = userID;
 	}
-	public String getUserAccount() {
-		return userAccount;
-	}
-	public void setUserAccount(String userAccount) {
-		this.userAccount = userAccount;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+	
 	public String getUserPassword() {
 		return userPassword;
 	}
+	
 	public void setUserPassword(String userPassword) {
 		this.userPassword = userPassword;
 	}
-	
-	
 }
--- src/main/java/com/fashiongo/cms/web/MyAccountController.java
+++ src/main/java/com/fashiongo/cms/web/MyAccountController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fashiongo.cms.common.JSONResponse;
@@ -21,8 +20,10 @@ public class MyAccountController {
 	
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public JSONResponse<ProcedureResult> save(@RequestBody MyAccountListParam myAccountListParam) throws Exception {
+		
 		JSONResponse<ProcedureResult> jsonResponse = new JSONResponse<>();
 		jsonResponse.setData(myAccountService.modifyUserPassword(myAccountListParam));
+		
 		return jsonResponse;
 	}
 }

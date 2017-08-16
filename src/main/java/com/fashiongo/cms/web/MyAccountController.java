7e9aef20bcc2813270026eb543c777a66460b70d
suhwan-Choi <suhwan-Choi@fashiongo.net>
2017-08-17 08:17:26 +0900

    [Mason] add MyAccountManager api
    -- get MyAccountInformation

--- src/main/java/com/fashiongo/cms/web/MyAccountController.java
+++ src/main/java/com/fashiongo/cms/web/MyAccountController.java
@@ -1,15 +1,26 @@
 package com.fashiongo.cms.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fashiongo.cms.service.MyAccountService;
+import com.fashiongo.cms.common.JSONResponse;
+import com.fashiongo.cms.model.UserManager;
+import com.fashiongo.cms.service.UserManagerService;
 
 @RestController
 @RequestMapping("/my_account")
 public class MyAccountController {
-	
+
 	@Autowired
-	private MyAccountService myAccountService;
+	private UserManagerService userManagerService;
+
+	@RequestMapping(value = "/detail/{userID}", method = RequestMethod.GET)
+	public JSONResponse<UserManager> detail(@PathVariable Integer userID) {
+		JSONResponse<UserManager> jsonResponse = new JSONResponse<>();
+		jsonResponse.setData(userManagerService.selectDetailUserManager(userID));
+		return jsonResponse;
+	}
 }

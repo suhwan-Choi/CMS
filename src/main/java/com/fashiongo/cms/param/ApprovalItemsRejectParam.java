4bd9f491c27a0875881b69a9baf39a4f071cd288
suhwan-Choi <suhwan-Choi@fashiongo.net>
2017-09-06 11:50:21 +0900

    [Mason] ApprovalItems
    -- add ApprovalItemsRejectParam

---
+++ src/main/java/com/fashiongo/cms/param/ApprovalItemsRejectParam.java
@@ -0,0 +1,34 @@
+package com.fashiongo.cms.param;
+
+public class ApprovalItemsRejectParam {
+	private Integer workedBy;
+	private String sessionKey;
+	private String rejectList;
+
+	public Integer getWorkedBy() {
+		return workedBy;
+	}
+
+	public void setWorkedBy(Integer workedBy) {
+		this.workedBy = workedBy;
+	}
+
+	public String getSessionKey() {
+		return sessionKey;
+	}
+
+	public void setSessionKey(String sessionKey) {
+		this.sessionKey = sessionKey;
+	}
+
+	public String getRejectList() {
+		return rejectList;
+	}
+
+	public void setRejectList(String rejectList) {
+		this.rejectList = rejectList;
+	}
+
+	
+
+}
